d6a0c86cac18526dcb13c29ff1bcc1811857ad2e
Jose_Bento_Abreu <163444089+JoseBento16@users.noreply.github.com>
2026-05-14 23:27:58 -0400

    Fix na tela _layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,15 @@
 import { Tabs } from "expo-router";
 
 export default function RootLayout(){
-  return (
-    <Tabs initialRouteName="index">
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="cadastro" />
-      <Tabs.Screen name="preview" />
-      <Tabs.Screen name="sucesso" />
-    </Tabs>
-  )
+    return (
+            <Tabs
+                initialRouteName="index" screenOptions={{
+                    headerShown: false,
+                }}>
+                <Tabs.Screen name="index" />
+                <Tabs.Screen name="cadastro" />
+                <Tabs.Screen name="preview" />
+                <Tabs.Screen name="sucesso" />
+            </Tabs>
+    )
 }
